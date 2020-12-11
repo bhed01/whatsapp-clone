ffd4c88d7c455d4b873e0a225e45ad35d5750992
Bhed Kumar Kushwaha <kushwaha.ved@gmail.com>
2020-12-11 21:06:15 +0530

    Added accidentally deleted firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {auth, provider};
+export default db;
+
+export const createRoom = () => {
+    const roomName = prompt('Please enter name for new room');
+    if (roomName) {
+        db.collection('rooms').add({
+            name: roomName
+        });
+    }
+};
